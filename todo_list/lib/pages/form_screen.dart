00761711aeef0b524efd23b27afa40bb03784b7c
Emanuel Sergio Girardi <emanuelsergiogirardi@gmail.com>
2025-08-20 14:56:32 -0300

    Add form fields and image preview to FormScreen
    Introduced TextEditingControllers and input fields for name, difficulty, and image URL in FormScreen. Added an image preview and an 'Adicionar' button to enhance the task creation UI.

--- todo_list/lib/pages/form_screen.dart
+++ todo_list/lib/pages/form_screen.dart
@@ -6,6 +6,10 @@ class FormScreen extends StatefulWidget {
 }
 
 class _FormScreenState extends State<FormScreen> {
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _difficultyController = TextEditingController();
+  TextEditingController _imageController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,7 +32,70 @@ class _FormScreenState extends State<FormScreen> {
             borderRadius: BorderRadius.circular(10),
             border: Border.all(color: Colors.black, width: 3),
           ),
-          child: Column(),
+          child: Column(
+            children: [
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      controller: _nameController,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Nome",
+                        fillColor: Colors.white,
+                        filled: true,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      controller: _difficultyController,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Dificuldade",
+                        fillColor: Colors.white,
+                        filled: true,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      controller: _imageController,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Imagem",
+                        fillColor: Colors.white,
+                        filled: true,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 72,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: Colors.blue, width: 2),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      child: Image.network(
+                        _imageController.text,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(onPressed: () {}, child: Text("Adicionar")),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
